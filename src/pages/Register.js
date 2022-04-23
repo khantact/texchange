10769d5abeda877c98cac471b5fc60f5990da941
Kevin Han <khan@colgate.edu>
2022-04-22 20:59:23 -0400

    fix

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -9,10 +9,14 @@ const Register = () => {
   const userRef = useRef();
   const errRef = useRef();
 
-  const[user, setUser] = useState('');
+  const [user, setUser] = useState('');
   const [validName, setValidName] = useState(false);
   const [userFocus, setUserFocus] = useState(false);
   
+ const [pwd, setPwd] = useState('');
+ const [validPwd, setValidPwd] = useState(false);
+ const [pwdFocus, setPwdFocus] = useState(false);
+
   const [matchPwd, setMatchPwd] = useState('');
   const [validMatch, setValidMatch] = useState(false);
   const [matchFocus, setMatchFocus] = useState(false);
