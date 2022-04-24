e33afd6b957e05c471b872384947cde49733cc3e
Kevin Han <khan@colgate.edu>
2022-04-24 18:15:29 -0400

    initial commit

---
+++ src/context/auth.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createContext, useEffect, useState } from 'react'
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({children}) =>{
+    return <AuthContext.Provider>{children}</AuthContext.Provider>
+}
+
+export default AuthProvider;
